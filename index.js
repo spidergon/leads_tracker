933d741e21e480ed7795bf81ed5cadf40b3cb189
spidergon <spidergon@gmail.com>
2021-08-13 15:12:11 -0300

    dev

--- index.js
+++ index.js
@@ -1,24 +1,40 @@
-let myLeads = []
-const inputEl = document.getElementById("input-el")
-const inputBtn = document.getElementById("input-btn")
-const ulEl = document.getElementById("ul-el")
+let myLeads = [];
 
-inputBtn.addEventListener("click", function() {
-    myLeads.push(inputEl.value)
-    inputEl.value = ""
-    renderLeads()
-})
+const inputEl = document.getElementById('input-el');
+const inputBtn = document.getElementById('input-btn');
+const deleteBtn = document.getElementById('delete-btn');
+const ulEl = document.getElementById('ul-el');
 
-function renderLeads() {
-    let listItems = ""
-    for (let i = 0; i < myLeads.length; i++) {
-        listItems += `
-            <li>
-                <a target='_blank' href='${myLeads[i]}'>
-                    ${myLeads[i]}
-                </a>
-            </li>
-        `
-    }
-    ulEl.innerHTML = listItems  
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem('myLeads'));
+
+if (leadsFromLocalStorage) {
+  myLeads = leadsFromLocalStorage;
+  render(myLeads);
 }
+
+function render(leads) {
+  let listItems = '';
+  for (let i = 0; i < leads.length; i++) {
+    listItems += `
+      <li>
+        <a target="_blank" href="${leads[i]}">
+          ${leads[i]}
+        </a>
+      </li>
+    `;
+  }
+  ulEl.innerHTML = listItems;
+}
+
+inputBtn.addEventListener('click', function () {
+  myLeads.push(inputEl.value);
+  inputEl.value = '';
+  localStorage.setItem('myLeads', JSON.stringify(myLeads));
+  render(myLeads);
+});
+
+deleteBtn.addEventListener('dblclick', function () {
+  localStorage.clear();
+  myLeads = [];
+  render(myLeads);
+});
