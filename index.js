87fb7162236311b7b603450133f6035e50779f72
spidergon <spidergon@gmail.com>
2021-08-13 17:41:21 -0300

    dev

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ let myLeads = [];
 
 const inputEl = document.getElementById('input-el');
 const inputBtn = document.getElementById('input-btn');
+const tabBtn = document.getElementById('tab-btn');
 const deleteBtn = document.getElementById('delete-btn');
 const ulEl = document.getElementById('ul-el');
 
@@ -31,6 +32,14 @@ inputBtn.addEventListener('click', function () {
   inputEl.value = '';
   localStorage.setItem('myLeads', JSON.stringify(myLeads));
   render(myLeads);
+});
+
+tabBtn.addEventListener('click', function () {
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    myLeads.push(tabs[0].url);
+    localStorage.setItem('myLeads', JSON.stringify(myLeads));
+    render(myLeads);
+  });
 });
 
 deleteBtn.addEventListener('dblclick', function () {
